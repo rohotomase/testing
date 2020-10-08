eba3b67272942bcfc175693cd9357e67eb41337a
rohotomase <scott.t.mooney@gmail.com>
2020-10-08 13:45:47 -0400

    test lgtm

--- testing.cpp
+++ testing.cpp
@@ -31,6 +31,8 @@ int main()
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dist(1, 10);
+	int arr[] = { 0, 1, 2 };
+	cout << arr[3] << std::endl;
 	int x;
 	Player player;
 	while (true)
